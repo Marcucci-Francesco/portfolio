a3aff5c85399e9db43ec59ffb032a2b447442b67
Marcucci-Francesco <fmarcucci47@gmail.com>
2025-07-31 21:08:16 +0200

    [feat] add pages

---
+++ src/pages/Aboutme.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Aboutme = () => {
+  return (
+    <>
+    </>
+  )
+}
+
+export default Aboutme
---
+++ src/pages/Contactspage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Contactspage = () => {
+  return (
+    <>
+    </>
+  )
+}
+
+export default Contactspage
