78a7f53a3c8ef23a1726c6e2028602ccd241134e
Marcucci-Francesco <fmarcucci47@gmail.com>
2025-08-04 19:47:31 +0200

    [feat] add first header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,22 @@ import React from 'react'
 const Header = () => {
   return (
     <header>
-      <h1 className='text-black'>Welcome to My Website</h1>
+      <div className='bg-dark' style={{ height: 60 }}>
+        <div className='d-flex justify-space-around'>
+          <div className='d-flex align-items-center' style={{ height: 60 }}>
+            <span className='text-white'>Qualcosa su di me</span>
+            <span className='text-white'>I miei progetti</span>
+          </div>
+          <div className='d-flex align-items-center' style={{ height: 60 }}>
+            <h1 className='text-warning'>PORTFOLIO</h1>
+          </div>
+          <div className='d-flex align-items-center' style={{ height: 60 }}>
+            <span className='text-white'>Contatti</span>
+            <span className='text-white'>Social</span>
+          </div>
+
+        </div>
+      </div>
     </header>
   )
 }
