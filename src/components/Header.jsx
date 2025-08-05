a553bd405a6e8395921b1e07192f057715b73004
Marcucci-Francesco <fmarcucci47@gmail.com>
2025-08-05 19:25:20 +0200

    [feat] header completed and start homepage

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,39 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCode } from '@fortawesome/free-solid-svg-icons'
 
 const Header = () => {
   return (
     <header>
-      <div className='glass-effect' style={{ height: 60 }}>
-        <div className='d-flex justify-content-around'>
-          <div className='d-flex align-items-center justify-content-around w-75 text-uppercase' style={{ height: 60 }}>
-            <Link to={'/project'} className='d-flex justify-content-center text-dark nav text-decoration-none w-25 fw-semibold'>I miei progetti</Link>
-            <Link to={'/aboutme'} className='d-flex justify-content-center text-dark nav text-decoration-none w-25 fw-semibold'>Qualcosa su di me</Link>
-            <Link to={'/contact'} className='d-flex justify-content-center text-dark nav text-decoration-none w-25 fw-semibold'>Contatti</Link>
+      <nav className="navbar navbar-expand-lg glass-effect position-fixed top-0 start-0 w-100" style={{ height: '60px', zIndex: 1000 }}>
+        <div className="container-fluid">
+          <Link className="navbar-brand fw-bold text-dark d-flex align-items-center gap-2" to="/">
+            <FontAwesomeIcon icon={faCode} size="lg" className='logo-hover' />
+            Francesco Marcucci
+          </Link>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+            <ul className="navbar-nav d-flex flex-row gap-4 text-uppercase fw-semibold">
+              <li className="nav-item">
+                <Link to="/aboutme" className="nav-link nav-link-hover text-white">Qualcosa su di me</Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/project" className="nav-link nav-link-hover text-white">I miei progetti</Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/contact" className="nav-link nav-link-hover text-white">Contatti</Link>
+              </li>
+            </ul>
           </div>
         </div>
-      </div>
+      </nav>
     </header>
-
   )
 }
 
 export default Header
+
+
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -2,9 +2,29 @@ import React from 'react'
 
 const Homepage = () => {
   return (
-    <>
-    </>
+    <main className="position-relative" style={{ height: '100vh' }}>
+
+      <div className="d-flex h-100">
+        <div className="bg-light" style={{ width: '33.33%', height: '100%' }}></div>
+        <div className="bg-dark" style={{ width: '66.66%', height: '100%' }}></div>
+      </div>
+
+      <div
+        className="bg-white position-absolute shadow rounded"
+        style={{
+          width: '1100px',
+          height: '350px',
+          top: '50%',
+          left: 'calc(33.33% - 250px)',
+          transform: 'translateY(-50%)',
+          zIndex: 10
+        }}
+      >
+      </div>
+    </main>
   )
 }
 
 export default Homepage
+
+
